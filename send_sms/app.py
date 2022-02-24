4220cc83c4209170b034b68bb1eb640b7cfa9380
punithareddy5 <saipunithaarumalla@gmail.com>
2022-02-24 16:41:21 +0000

    deskcommit

---
+++ send_sms/app.py
@@ -0,0 +1,22 @@
+import json
+import uuid
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    # print (event)
+    body = json.loads(event['Records'][0]['body'])
+    Message = body['Message']
+    Message_loads = json.loads(Message)
+
+    number = Message_loads['number']
+    message = Message_loads['message']
+
+    
+    sns_client = boto3.client('sns')
+    aws_account_id = context.invoked_function_arn.split(":")[4]
+    aws_region = context.invoked_function_arn.split(":")[3]
+    response = sns_client.publish(
+    PhoneNumber='{0}{1}'.format('+91', number),
+    Message= message
+    )
---
+++ send_sms/app2.py
@@ -0,0 +1,20 @@
+import json
+import uuid
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    body = json.loads(event['body'])
+    number = body['number']
+    message = body['message']
+    output = {"number": number, "message": message, "request_id": str(uuid.uuid1())}
+    json_dump = json.dumps(output)
+
+    
+    sns_client = boto3.client('sns')
+    aws_account_id = context.invoked_function_arn.split(":")[4]
+    aws_region = context.invoked_function_arn.split(":")[3]
+    response = sns_client.publish(
+    TopicArn='arn:aws:sns:{0}:{1}:SendSMSTopic'.format(aws_region, aws_account_id),
+    Message= json_dump
+    )
